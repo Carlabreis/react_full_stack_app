cd14c61af1559c760c05a30e20e03f25b49f7c5d
Carla Borges Reis <carlareis.dev@gmail.com>
2023-12-09 14:23:15 -0800

    error handling at courses.js

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,47 +1,66 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+import { api } from "../utils/apiHelper";
 
 const Courses = () => {
-    const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/courses")
-      .then((res) => res.json())
-      .then((result) => {
-        setCourses(result);
-      });
-  }, []);
+    const fetchData = async () => {
+      try {
+        const response = await api('/courses', 'GET');
+        const coursesJson = await response.json();
+
+        if (response.status === 200) {
+          setCourses(coursesJson);
+        } else if (response.status === 500) {
+          navigate('/error');
+        }
+      } catch (error) {
+        console.log('Error fetching and parsing courses data', error);
+        navigate('/error');
+      }
+    }
+    fetchData();
+  }, [navigate]);
+
 
   return (
     <main>
       <div className="wrap main--grid">
-      {courses.map((course) => (
-        <Link
-          key={course.id}
-          className="course--module course--link"
-          to={`/courses/${course.id}`}
-        >
-          <h2 className="course--label">Course</h2>
-          <h3 className="course--title">{course.title}</h3>
-        </Link>
-      ))}
-      <Link className="course--module course--add--module" to="/courses/create">
-        <span className="course--add--title">
-          <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            viewBox="0 0 13 13"
-            className="add"
+        {courses.map((course) => (
+          <Link
+            key={course.id}
+            className="course--module course--link"
+            to={`/courses/${course.id}`}
           >
-            <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-          </svg>
-          New Course
-        </span>
-      </Link>
-    </div>
+            <h2 className="course--label">Course</h2>
+            <h3 className="course--title">{course.title}</h3>
+          </Link>
+        ))}
+        <Link
+          className="course--module course--add--module"
+          to="/courses/create"
+        >
+          <span className="course--add--title">
+            <svg
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              viewBox="0 0 13 13"
+              className="add"
+            >
+              <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+            </svg>
+            New Course
+          </span>
+        </Link>
+      </div>
     </main>
   );
 };
