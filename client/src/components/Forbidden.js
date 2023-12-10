538266849acecc181e8454ca31401ecbe864ac5c
Carla Borges Reis <carlareis.dev@gmail.com>
2023-12-10 09:25:39 -0800

    updatecourse functionality and forbidden.js className

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -1,7 +1,7 @@
 const Forbidden = () => {
   return (
     <main>
-      <div class="wrap">
+      <div className="wrap">
         <h2>Forbidden</h2>
         <p>Oh oh! You can't access this page.</p>
       </div>
--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,25 +1,76 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import UserContext from "../context/UserContext";
+import { api } from "../utils/apiHelper";
+import ErrorsDisplay from "./ErrorsDisplay";
 
 const UpdateCourse = () => {
   const { authUser } = useContext(UserContext);
   const { id } = useParams();
   const navigate = useNavigate();
 
+  // State
+  const title = useRef(null);
+  const description = useRef(null);
+  const estimatedTime = useRef(null);
+  const materialsNeeded = useRef(null);
   const [course, setCourse] = useState([]);
+  const [errors, setErrors] = useState([]);
+
+  // Fetch course data and set course on load
 
   useEffect(() => {
-    fetch(`http://localhost:5000/api/courses/${id}`)
-      .then((res) => res.json())
-      .then((result) => {
-        setCourse(result);
-      });
-  }, [id]);
+    const fetchData = async () => {
+      try {
+        const response = await api(`/courses/${id}`, 'GET')
+        const courseJson = await response.json();
 
-  const handleSubmit = () => {
-    console.log(course);
-  }
+        if (response.status === 200) {
+          if (authUser.id === courseJson.userId) { // if user logged in is same as course author
+            setCourse(courseJson);
+          } else { // forbidden
+            navigate("/forbidden");
+          }
+        } else {
+          throw new Error();
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData();
+  }, [id, authUser.id, navigate]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const currentCourse = {
+      title: title.current.value,
+      description: description.current.value,
+      estimatedTime: estimatedTime.current.value,
+      materialsNeeded: materialsNeeded.current.value,
+      userId: authUser.id,
+    };
+
+    try {
+      const response = await api(`/courses/${id}`, "PUT", currentCourse, authUser);
+
+      if (response.status === 204) {
+        console.log(`${course.title} is successfully updated!`);
+        navigate(`/courses/${id}`)
+      } else if (response.status === 400) { // bad request (e.i. wrong name or password)
+        const data = await response.json();
+        setErrors(data.errors);
+      } else if (response.status === 403) { // forbidden 
+        navigate("/forbidden")
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      console.log(error);
+      navigate("/error");
+    }
+  };
 
   const handleCancel = (event) => {
     event.preventDefault();
@@ -30,6 +81,7 @@ const UpdateCourse = () => {
     <main>
       <div className="wrap">
         <h2>Update Course</h2>
+        <ErrorsDisplay errors={errors} />
         <form onSubmit={handleSubmit}>
           <div className="main--flex">
             <div>
@@ -38,8 +90,8 @@ const UpdateCourse = () => {
                 id="courseTitle"
                 name="courseTitle"
                 type="text"
-                // value={course.title == null ? "" : course.title}
-                placeholder={course.title}
+                ref={title}
+                defaultValue={course.title == null ? "" : course.title}
               />
 
               <p>By {authUser.firstName + " " + authUser.lastName}</p>
@@ -48,8 +100,8 @@ const UpdateCourse = () => {
               <textarea
                 id="courseDescription"
                 name="courseDescription"
-                // value={course.description}
-                placeholder={course.description}
+                ref={description}
+                defaultValue={course.description}
               />
             </div>
             <div>
@@ -58,26 +110,25 @@ const UpdateCourse = () => {
                 id="estimatedTime"
                 name="estimatedTime"
                 type="text"
-                // value={course.estimatedTime == null ? "" : course.estimatedTime}
-                placeholder={course.estimatedTime}
+                ref={estimatedTime}
+                defaultValue={
+                  course.estimatedTime == null ? "" : course.estimatedTime
+                }
               />
 
               <label htmlFor="materialsNeeded">Materials Needed</label>
               <textarea
                 id="materialsNeeded"
                 name="materialsNeeded"
-                // value={course.materialsNeeded}
-                placeholder={course.materialsNeeded}
+                ref={materialsNeeded}
+                defaultValue={course.materialsNeeded}
               />
             </div>
           </div>
           <button className="button" type="submit">
             Update Course
           </button>
-          <button
-            className="button button-secondary"
-            onClick={handleCancel}
-          >
+          <button className="button button-secondary" onClick={handleCancel}>
             Cancel
           </button>
         </form>
